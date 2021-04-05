30bd98f5b2bc67fdb439f7f700a4a5d6d7b1f100
Greg Hayes <gthelight@gmail.com>
2021-04-05 18:07:44 -0400

    moved .env to root of project

--- e-com-mui/src/App.js
+++ e-com-mui/src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { commerce } from "./lib/commerce";
 
 // import Products from "./components/Products/Products";
 // import Navbar from './components/Navbar/Navbar';
@@ -6,6 +7,19 @@ import React from "react";
 import { Products, Navbar } from "./components";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <div>
       <Navbar />
