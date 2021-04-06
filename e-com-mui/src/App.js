1c8aae93b48f7c6dd08a00473c4842abae7084eb
Greg Hayes <gthelight@gmail.com>
2021-04-05 23:58:33 -0400

    added cart

--- e-com-mui/src/App.js
+++ e-com-mui/src/App.js
@@ -8,17 +8,23 @@ import { Products, Navbar } from "./components";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
     setProducts(data);
   };
 
+  const fetchCart = async () => {
+    // This skips creating a cart variable and immediately uses API response (promise)
+    setCart(await commerce.cart.retrieve());
+  };
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
-
-  console.log(products);
+  console.log(cart);
 
   return (
     <div>
